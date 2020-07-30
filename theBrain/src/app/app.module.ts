b9d754d69f2b89c1a105a31a402f2a701bd06ce1
Fabio Calegari <calegari2711@gmail.com>
2020-07-29 22:14:26 -0300

    Add first component and a random method

--- theBrain/src/app/app.module.ts
+++ theBrain/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataBindingComponent,
   ],
   imports: [
     BrowserModule,
